2aa4b91f3569611730910d8f98e0306e4fb9e7f1
Sergey Shandar <sergey@live.com.au>
2017-04-10 14:17:10 -0700

    Remote Server implementation.

--- Rest.ClientRuntime.Test/Rest.ClientRuntime.Test/JsonRpc/RemoteServer.cs
+++ Rest.ClientRuntime.Test/Rest.ClientRuntime.Test/JsonRpc/RemoteServer.cs
@@ -1,13 +1,49 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Rest.ClientRuntime.Test.Rpc;
+using Rest.ClientRuntime.Test.TextRpc;
+using System;
 
 namespace Rest.ClientRuntime.Test.JsonRpc
 {
     public class RemoteServer : IServer
     {
+        private readonly IMarshalling _Marshalling;
+
+        private readonly Utf8Reader _Reader;
+
+        private readonly Utf8Writer _Writer;
+
+        public RemoteServer(Utf8Reader reader, Utf8Writer writer, IMarshalling marshalling)
+        {
+            _Reader = reader;
+            _Writer = writer;
+            _Marshalling = marshalling;
+        }
+
         public async Task<T> Call<T>(string method, Dictionary<string, object> @params)
         {
-            return default(T);
+            _Writer.WriteMessage(
+                _Marshalling,
+                new Request 
+                {
+                    method = method,
+                    @params = @params,
+                });
+            Response<T> response;
+            while (true)
+            {
+                response = _Reader.ReadMessage<Response<T>>(_Marshalling);
+                if (response != null)
+                {
+                    break;
+                }
+            }
+            if (response.error != null)
+            {
+                throw new Exception(response.error.message);
+            }
+            return response.result;
         }
     }
 }
