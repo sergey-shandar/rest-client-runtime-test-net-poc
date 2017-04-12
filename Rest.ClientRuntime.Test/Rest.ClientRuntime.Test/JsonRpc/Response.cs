1cbe816c75ae78774a1df8ef2625863034bd7cd1
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2017-04-12 10:47:15 -0700

    Update Response.cs

--- Rest.ClientRuntime.Test/Rest.ClientRuntime.Test/JsonRpc/Response.cs
+++ Rest.ClientRuntime.Test/Rest.ClientRuntime.Test/JsonRpc/Response.cs
@@ -1,8 +1,14 @@
-﻿namespace Rest.ClientRuntime.Test.JsonRpc
+namespace Rest.ClientRuntime.Test.JsonRpc
 {
     public sealed class Response<T> : Message
     {
         public T result { get; set; }
         public Error error { get; set; }
+
+        public Response(int id, T result, Error error): base(id)
+        {
+            this.result = result;
+            this.error = error;
+        }
     }
 }
