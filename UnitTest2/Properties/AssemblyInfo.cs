c7e48df9ab362f0b3cd7d5b605c47ba441cdad0a
Sergey Shandar <sergey@live.com.au>
2017-04-18 14:43:35 -0700

    .Net Standard 1.5

--- UnitTest2/Properties/AssemblyInfo.cs
+++ UnitTest2/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Rest.ClientRuntime.Test.UnitTest")]
+[assembly: AssemblyTitle("UnitTest2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rest.ClientRuntime.Test.UnitTest")]
+[assembly: AssemblyProduct("UnitTest2")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("0cb184cf-40c7-4237-a022-d598802ff590")]
+[assembly: Guid("e5092d77-2bed-4694-985a-19303282c8bb")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
