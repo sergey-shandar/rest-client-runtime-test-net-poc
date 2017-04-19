7b36e46548e87d09ec109e74aac941f2cf43cba9
Sergey Shandar <sergey@live.com.au>
2017-04-19 11:45:41 -0700

    add logging files.

---
+++ Microsoft.Rest.ClientRuntime.Test/Log/ReaderAndLog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Rest.ClientRuntime.Test.Utf8;
+using System.IO;
+
+namespace Microsoft.Rest.ClientRuntime.Test.Log
+{
+    public sealed class ReaderAndLog : IReader
+    {
+        private readonly IReader _Reader;
+
+        private readonly Action<string> _Log;
+
+        public ReaderAndLog(IReader reader, Action<string> log)
+        {
+            _Reader = reader;
+            _Log = log;
+        }
+
+        public ReaderAndLog(IReader reader, Stream stream): this(reader, stream.ToLog())
+        { 
+        }
+
+        public string ReadBlock(int length)
+        {
+            var result = _Reader.ReadBlock(length);
+            _Log(result);
+            return result;
+        }
+
+        public string ReadLine()
+        {
+            var result = _Reader.ReadLine();
+            _Log(result + Writer.Eol);
+            return result;
+        }
+    }
+}
---
+++ Microsoft.Rest.ClientRuntime.Test/Log/WriterAndLog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Rest.ClientRuntime.Test.Utf8;
+using System.IO;
+
+namespace Microsoft.Rest.ClientRuntime.Test.Log
+{
+    public sealed class WriterAndLog : IWriter
+    {
+        private readonly IWriter _Writer;
+
+        private Action<string> _Log;
+
+        public WriterAndLog(IWriter writer, Action<string> log)
+        {
+            _Writer = writer;
+            _Log = log;
+        }
+
+        public WriterAndLog(IWriter writer, Stream stream): this(writer, stream.ToLog())
+        {
+        }
+
+        public void Flush()
+        {
+            _Writer.Flush();
+        }
+
+        public IWriter Write(string value)
+        {
+            _Log(value);
+            _Writer.Write(value);
+            return this;
+        }
+    }
+}
