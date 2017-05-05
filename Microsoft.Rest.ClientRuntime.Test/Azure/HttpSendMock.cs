11438e55dcdc1cc08aa9da12be8f9286be7a7951
Sergey Shandar <sergey@live.com.au>
2017-05-05 16:57:46 -0700

    Kill the server.

--- Microsoft.Rest.ClientRuntime.Test/Azure/HttpSendMock.cs
+++ Microsoft.Rest.ClientRuntime.Test/Azure/HttpSendMock.cs
@@ -93,10 +93,11 @@ namespace Microsoft.Rest.ClientRuntime.Test.Azure
             };
 
             Result<object> response;
-            using (var process = StartProcess())
+            using(var process = StartProcess())
             {
                 var remoteServer = new RemoteServer(process.CreateIo(), new Marshalling(null, null));
                 response = await remoteServer.Call<Result<object>>(request.Method.Method, @params);
+                process.Kill();
             }            
             
             return new HttpResponseMessage(response.statusCode)
